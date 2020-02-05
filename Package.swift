734ebd47e79c302941a54c10f1c2208935c85b35
kreiszistvan <kreiszdev@gmail.com>
2020-02-05 23:21:05 +0100

    updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,24 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "CombineRealm",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "CombineRealm",
             targets: ["CombineRealm"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .upToNextMajor(from: "4.3.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CombineRealm",
-            dependencies: []),
-        .testTarget(
-            name: "CombineRealmTests",
-            dependencies: ["CombineRealm"]),
+            dependencies: ["Realm", "RealmSwift"],
+            path: "Sources"
+        )
     ]
 )
